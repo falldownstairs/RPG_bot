8494986105e2cb30078f6acefe26715b1d54d251
eric11055 <ec2020canada@gmail.com>
2022-05-21 17:06:17 -0700

    added commands

--- rpgbot.py
+++ rpgbot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 client = discord.Client()
 
@@ -12,6 +13,45 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send("hello!")
+
+@bot.command()
+async def test(ctx,startgame):
+    pass
+
+@bot.command()
+async def test(ctx,help):
+    pass
+
+@bot.command()
+async def test(ctx,savegame):
+    pass
+
+@bot.command()
+async def test(ctx,loadgame):
+    pass
+@bot.command()
+async def test(ctx,option1):
+    pass
+@bot.command()
+async def test(ctx,option2):
+    pass
+@bot.command()
+async def test(ctx,option3):
+    pass
+@bot.command()
+async def test(ctx,option4):
+    pass
+@bot.command()
+async def test(ctx,aboutbot):
+    pass
+@bot.command()
+async def test(ctx,storysynposis):
+    pass
+
+
+
+
+
 
 client.run("OTc1MTg1OTkwMTY5ODA0ODIw.GtRKgx.GAOuK6l6gXK6QP9sIth33Qf2Z-vXo4EHBfrZcU")
