3d5415f61f92917f9a7a1be7a28f6661f039bd55
jacob <jacoblacsamana0@gmail.com>
2022-05-30 16:22:47 -0700

    implemented game saving

--- game.py
+++ game.py
@@ -1,7 +1,13 @@
 #represents a game
 
-class game():
+class Game():
 
 	def __init__(self, thread_id, player_id):
 		self.thread_id = thread_id
 		self.player_id = player_id
+
+	def getTid(self):
+		return self.thread_id
+
+	def getPid(self):
+		return self.player_id
--- rpgbot.py
+++ rpgbot.py
@@ -1,13 +1,24 @@
 import discord
+import pickle
+from game import Game
 from discord.ext import commands
+import os
 
 cmd_intents = discord.Intents.default()
 cmd_intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents = cmd_intents) # You can change the command prefix to whatever you want.
 
+games = []
+
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+    
+    #create games save file if one doesn't exist
+    saves = open( "save.p", "wb+" )
+
+    #load from save file
+    games = pickle.load(saves)
 
 # for debug purposes
 @bot.command() 
@@ -19,16 +30,21 @@ async def ping(ctx):
 @bot.command() 
 async def shutdown(ctx): 
     await ctx.send("Stopping bot") 
+    pickle.dump(games, open( "save.p", "rb+" ))
     exit()
 
 @bot.command(name = "start", description = "starts a game")
 async def startgame(ctx):
-    await ctx.channel.create_thread(name= ctx.author.name + "'s" + "game", auto_archive_duration=60, type=discord.ChannelType.public_thread, reason = None)
+    new_game = await ctx.channel.create_thread(name= ctx.author.name + "'s" + "game", auto_archive_duration=60, type=discord.ChannelType.public_thread, reason = None)
+    games.append(Game(new_game.id, ctx.author.id))
     
 # clears all game threads
-@bot.command()
-async def clearGames():
-    
+@bot.command(name="clear", description= "clears all ongiong game threads")
+async def clearGames(ctx):
+    for game in games:
+        thread = bot.get_channel(game.getTid())
+        await thread.delete()
+    games.clear()
 
 @bot.command()
 async def info(ctx):
